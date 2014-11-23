2183be1eb1659f0efdde5e479da557461fd02c79
Saumitra Aditya <saumitraaditya@users.noreply.github.com>
2014-11-23 16:37:32 -0500

    Update twiitter_client_V2.java
    fully formatted data, uploads files to S3buckets

--- twiitter_client_V2.java
+++ twiitter_client_V2.java
@@ -6,12 +6,26 @@
 
 package twitter_Augur;
 
+
 import twitter4j.*;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class twitter_client extends Client_Builder {
+	
+   	public static final String TEXT_DATA = "T";
+	public static final String NUMERIC_DATA = "N";
+	public static final String TWITTER = "T";
+	public static final String CRITIC = "C";
+	public static final String AUDIENCE = "A";
+	public static final String FILENAME = "Twitter.txt";
+	
 	public QueryResult getReview(String movie_name,long max_id) throws TwitterException {
-		Query query = new Query( movie_name + " movie");
+		Query query = new Query( movie_name + " movie" +" -filter:retweets -filter:links -filter:replies -filter:images");
 		query.setCount(100);
 		query.setLang("en");
 		if (max_id != -1)
@@ -50,9 +64,8 @@ public class twitter_client extends Client_Builder {
 		long last_id = last_status.getId();
 		long max_id = last_id;
 		int count = 1;
-		
 		reviews.addAll(extractTweets(initial));
-		while (size > 99 && count < 3)
+		while (size > 99 && count < 30)
 		{
 			initial =  getReview(movie_name,max_id);
 			size = initial.getTweets().size();
@@ -64,23 +77,66 @@ public class twitter_client extends Client_Builder {
 		}
 		
 		return reviews;
-		
-		
-		
+			
 	}
-	public static void main(String[] args) {
-		twitter_client t = new twitter_client();
-		List<String> reviews = new LinkedList<String>();
-		try {
-			 reviews = t.getAllReviews("gone girl");
-		} catch (TwitterException x) {
+	
+	   public  String generateOutputFile(String movie_name) throws Exception
+	      {
+	    	String Filename = movie_name;
+			File inputFile = new File(Filename);
+			FileWriter fileWriter;
+			List <String> reviews = getAllReviews( movie_name);
+			String commentMeta = movie_name + "\t" + TEXT_DATA + 
+					 TWITTER  + AUDIENCE + " ";	
+			try {
 
+				inputFile.createNewFile();
+				fileWriter = new FileWriter(inputFile);
+				BufferedWriter bw = new BufferedWriter(fileWriter);
+				for (String review : reviews) {
+					String data = commentMeta + review;
+					bw.write(data);
+					bw.newLine();
+				}
+				for (String review : reviews) {
+					String data = commentMeta + review;
+					bw.write(data);
+					bw.newLine();
+				}
+				bw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			return Filename;
 		}
-		System.out.println(reviews.size());
-		for (int i = 0;i<reviews.size();i++)
-		{
-			System.out.println(reviews.get(i));
-		}
+	public static void main(String[] args) throws Exception
+	{
+		if (args.length < 3)
+    		{
+			System.out.println("Usage:javac -c jar <bucket-name> <folder> <movie1> <movie2>...<movie-n>");
+			throw new UnsupportedOperationException();
+    		}
+		twitter_client t = new twitter_client();
+		String bucket_name = args[0];
+    	S3Utility s3client = new S3Utility(bucket_name,args[1]);
+    	int arg_count = 2;
+    	String filename;
+    	List<String> movies = new ArrayList<String>();
+    	while (arg_count < args.length)
+    	{
+    		String temp_movie = args[arg_count].replace("_", " ");//for anirrudha's code.
+    		movies.add(temp_movie);
+    		arg_count++;
+    	}
+    	//System.out.println((movies.toString()));
+    	for (int i = 0;i < movies.size();i++)
+    	{
+    		Thread.sleep(100000);//sleep for 100 seconds, so that API limit is not violated.
+    		filename =   t.generateOutputFile(movies.get(i));
+    		s3client.uploadFile(filename);
+   
+    	}
 	}
 
 }
